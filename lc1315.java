356bf9dbe95e0e61e71608a9dc0947298e7721b5
Bowen Zhao <bzhao0106@gmail.com>
2023-09-08 16:06:47 +1000

    Update lc1315.java

--- lc1315.java
+++ lc1315.java
@@ -15,6 +15,31 @@
  */
 class Solution {
     public int sumEvenGrandparent(TreeNode root) {
-        
+        travse(root);
+        return sum;
+    }
+    int sum = 0;
+    void travse(TreeNode root) {
+        if (root == null) return;
+        if(root.val % 2 == 0) {
+            if (root.left != null) {
+                if (root.left.left != null) {
+                    sum += root.left.left.val;
+                }
+                if (root.left.right != null) {
+                    sum += root.left.right.val;
+                }
+            }
+            if (root.right != null) {
+                if (root.right.left != null) {
+                    sum += root.right.left.val;
+                }
+                if (root.right.right != null) {
+                    sum += root.right.right.val;
+                }
+            }
+        }
+        travse(root.left);
+        travse(root.right);
     }
 }
