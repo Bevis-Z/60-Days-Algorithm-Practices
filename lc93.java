664d43b5cb6f0aa6acfff12e924f73c6d7636429
Bowen Zhao <bzhao0106@gmail.com>
2023-09-23 10:24:20 +1000

    Create lc93.java

---
+++ lc93.java
@@ -0,0 +1,50 @@
+class Solution {
+    List<String> result = new ArrayList<>();
+
+    public List<String> restoreIpAddresses(String s) {
+        if (s.length() > 12) return result; // 算是剪枝了
+        backTrack(s, 0, 0);
+        return result;   
+    }
+
+    private void backTrack(String s, int startIndex, int pointNum) {
+        if (pointNum == 3) {
+            if (isValid(s,startIndex,s.length()-1)) {
+                result.add(s);
+            }
+            return;
+
+        }
+        for (int i = startIndex; i < s.length(); i++) {
+            if (isValid(s, startIndex, i)) {
+                s = s.substring(0, i + 1) + "." + s.substring(i + 1);    
+                pointNum++;
+                backTrack(s, i + 2, pointNum);
+                pointNum--;// 回溯
+                s = s.substring(0, i + 1) + s.substring(i + 2);// 回溯删掉逗点
+            } else {
+                break;
+            }
+        }
+    }
+
+    private Boolean isValid(String s, int start, int end) {
+        if (start > end) {
+            return false;
+        }
+        if (s.charAt(start) == '0' && start != end) { // 0开头的数字不合法
+            return false;
+        }
+        int num = 0;
+        for (int i = start; i <= end; i++) {
+            if (s.charAt(i) > '9' || s.charAt(i) < '0') { // 遇到⾮数字字符不合法
+                return false;
+            }
+            num = num * 10 + (s.charAt(i) - '0');
+            if (num > 255) { // 如果⼤于255了不合法
+                return false;
+            }
+        }
+        return true;
+    }
+}
