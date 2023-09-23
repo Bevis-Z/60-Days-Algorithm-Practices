cafcb721cf3dfdf8292a3115272a1357cb31206b
Bowen Zhao <bzhao0106@gmail.com>
2023-09-23 13:45:24 +1000

    Study Record

---
+++ lc78.java
@@ -0,0 +1,17 @@
+class Solution {
+    LinkedList<Integer> path = new LinkedList<>();
+    List<List<Integer>> result = new ArrayList<>(); 
+    public List<List<Integer>> subsets(int[] nums) {
+        backTracking(nums, 0);
+        return result;
+    }
+
+    private void backTracking(int[] nums, int startIndex) {
+        result.add(new ArrayList<>(path));
+        for (int i = startIndex; i < nums.length; i++) {
+            path.add(nums[i]);
+            backTracking(nums, i+1);
+            path.removeLast();
+        }
+    }
+}
