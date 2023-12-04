1d16e4be8f16173b25233d77a3a7559856cd5a7a
Bowen Zhao <bzhao0106@gmail.com>
2023-12-04 22:14:09 +1100

    Create lc509.java

---
+++ lc509.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0 || n==1) return n;
+
+        int a = 0, b = 1;
+        int result;
+        for(int i = 2; i <= n; i++){
+            result = a + b;
+            a = b;
+            b = result;
+        }
+        return b;
+    }
+}
