5b0f1c2f540beb9c7f4059f28b4a81c5d582866e
Bowen Zhao <bzhao0106@gmail.com>
2023-12-31 11:21:04 +1100

    Study Record

---
+++ lc24.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dumyhead = new ListNode(-1);
+        dumyhead.next = head;
+        ListNode cur = dumyhead;
+        ListNode temp;
+        ListNode firstNode;
+        ListNode secondNode;
+
+        while(cur.next != null && cur.next.next != null) {
+            temp = cur.next.next.next;
+            firstNode = cur.next;
+            secondNode = cur.next.next;
+            cur.next = secondNode;
+            secondNode.next = firstNode;
+            firstNode.next = temp;
+            cur = firstNode;
+        }
+        return dumyhead.next;
+    }
+}
