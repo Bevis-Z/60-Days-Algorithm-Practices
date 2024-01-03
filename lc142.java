6f6976ffc87aac91b92a0344d53862b4de0341fb
Bowen Zhao <bzhao0106@gmail.com>
2024-01-03 15:28:30 +1100

    Update lc142.java

--- lc142.java
+++ lc142.java
@@ -1,3 +1,28 @@
-public class lc142 {
-    
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> nodeSeen = new HashSet<>();
+
+        ListNode resultNode = head;
+        while (resultNode != null) {
+            if (nodeSeen.contains(resultNode)) {
+                return resultNode;
+            } else {
+                nodeSeen.add(resultNode);
+                resultNode = resultNode.next;
+            }
+        }
+
+        return null;
+    }
 }
