cd833b0569b72070856615d3c1ab1c7578e14fdb
Bowen Zhao <bzhao0106@gmail.com>
2024-01-04 17:23:16 +1100

    Update lc242.java

--- lc242.java
+++ lc242.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] record = new int[26];
-        for(int i = 0; i < s.length(); i++) {
-            record[s.charAt(i) - 'a']++;
+        int[] count = new int[26];
+        for (char c: s.toCharArray()) {
+            count[c - 'a']++;
         }
-        for(int i = 0; i < t.length(); i++) {
-            record[t.charAt(i) - 'a']--;
+        for (char c: t.toCharArray()) {
+            count[c - 'a']--;
         }
-        for(int i = 0; i < record.length; i++) {
-            if (record[i] != 0) return false;
+        for (int i: count) {
+            if (i != 0) {
+                return false;
+            }
         }
         return true;
     }
