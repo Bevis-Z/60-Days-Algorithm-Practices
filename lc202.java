979bf2dafd25909639f65829760c5c6fb42031dd
Bowen Zhao <bzhao0106@gmail.com>
2024-01-08 15:25:44 +1100

    Update lc202.java

--- lc202.java
+++ lc202.java
@@ -1,20 +1,21 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> record = new HashSet<>();
-        while (n != 1 && !record.contains(n)) {
-            record.add(n);
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
             n = getNextNumber(n);
         }
         return n == 1;
     }
 
     private int getNextNumber(int n) {
-        int res = 0;
-        while (n > 0) {
-            int temp = n % 10;
-            res += temp * temp;
-            n = n / 10;
+        int result = 0; 
+        int temp;       
+        while (n != 0) {
+            temp = n % 10;
+            result += temp * temp;
+            n /= 10;
         }
-        return res; 
+        return result;
     }
 }
