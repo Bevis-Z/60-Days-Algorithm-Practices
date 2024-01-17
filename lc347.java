19a5484e9b79432baaf543affe26cbb947cdc311
Bowen Zhao <bzhao0106@gmail.com>
2024-01-17 12:15:26 +1100

    Update lc347.java

--- lc347.java
+++ lc347.java
@@ -2,12 +2,12 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         if (k == nums.length) return nums;
         Map<Integer, Integer> count = new HashMap<>();
-        
         for (int num : nums) {
             count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
         List<Integer>[] buckets = new List[nums.length + 1];
+
         for (int i = 0; i < buckets.length; i++) {
             buckets[i] = new ArrayList<>();
         }
@@ -15,15 +15,12 @@ class Solution {
             buckets[count.get(key)].add(key);
         }
 
-        List<Integer> flattend = new ArrayList<>();
-        for (int i = buckets.length - 1; i >= 0; i--) {
-            for (int num : buckets[i]) {
-                flattend.add(num);
-            }
-        }
         int[] top = new int[k];
-        for (int i = 0; i < k; i++) {
-            top[i] = flattend.get(i);
+        int index = 0;
+        for (int i = buckets.length - 1; i >= 0 && index < k; i--) {
+            for (int num : buckets[i]) {
+                top[index++] = num;
+            }
         }
         return top;
     }
